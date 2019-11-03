ab2da1b99dfb3d31d9d6524c45c07cccc4605689
hstandeffer <hcolestandeffer@gmail.com>
2019-11-03 01:05:04 -0500

    uibuild

---
+++ build/precache-manifest.2c40a3f7ec0d43fe51e607aa0a509376.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105a911673871e9d3aa6d946b96267db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ffaa2283cce6487358",
+    "url": "/static/js/2.25f7c266.chunk.js"
+  },
+  {
+    "revision": "c7c65f68a2e7a49d74c5",
+    "url": "/static/js/main.f70a2dda.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
