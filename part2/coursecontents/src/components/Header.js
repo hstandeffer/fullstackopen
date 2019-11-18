8fa5afce1bb7e094721156b2ce403736bedaddac
hstandeffer <hcolestandeffer@gmail.com>
2019-11-17 22:58:38 -0600

    thru 3.18

--- part2/coursecontents/src/components/Header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Header = ({ name }) => <h1>{name}</h1>
-
-export default Header
--- part2/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = (props) => (
-  <div>
-    search: <input onChange={props.handleSearch} />
-  </div>
-)
-
-export default Filter
